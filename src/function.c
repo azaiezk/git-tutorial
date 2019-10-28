d05a60364b1661b627575a7331381e3b90e1b30c
azaiezk <khaled.azaiez@tum.de>
2019-10-28 16:28:16 +0100

    function updated

--- src/function.c
+++ src/function.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
-
+#include "espl_lib.h"
 int main() {
-
-	printf("Hello ESPL!");
-
-
-
+	int confirmed = 1;
+	do{
+		int nbr;
+		char msg;
+		printf("Hello ESPL! \n");
+		printf("write a number, please \n");
+		scanf("%d",&nbr);
+		getchar();
+		printf("%s",num_to_words(nbr)); 
+		printf(" \n Do you wish to continue ?(Y)es or (N)o? \n");
+		scanf("%c",&msg);
+		if ( msg == 'N'){
+			confirmed =0; 
+		}
+	} while (confirmed==1);
 } 
 
 
